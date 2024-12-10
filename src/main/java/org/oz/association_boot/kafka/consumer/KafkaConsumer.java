b95bf70efc63d1e5cc0df19710f234188c9fb2a4
minjae <wlstod98@gmail.com>
2024-12-10 19:44:56 +0900

    카프카 2차 테스트

--- src/main/java/org/oz/association_boot/kafka/consumer/KafkaConsumer.java
+++ src/main/java/org/oz/association_boot/kafka/consumer/KafkaConsumer.java
@@ -4,13 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.oz.association_boot.applier.domain.ApplierEntity;
-import org.oz.association_boot.applier.repository.ApplierEntityRepository;
-import org.oz.association_boot.kafka.dto.AuthConsumeDTO;
-import org.oz.association_boot.kafka.dto.AuthProducerDTO;
-import org.oz.association_boot.kafka.dto.TestDTO;
-import org.oz.association_boot.kafka.producer.KafkaAssociationProducer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.oz.association_boot.kafka.producer.KafkaCreatorProducer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -20,53 +14,34 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumer {
 
     private final ObjectMapper objectMapper;
-    private final KafkaAssociationProducer associationProducer;
-    private final ApplierEntityRepository applierEntityRepository;
+    private final KafkaCreatorProducer kafkaCreatorProducer;
 
-    @KafkaListener(topics = "sample-topic")
-    public void consumeMessage(ConsumerRecord<String, String> record) {
-        log.info("CONSUME TEST====================================================");
-        log.info("Consumed message: " + record);
-
-        try {
-            // 메시지 값을 JSON으로 받음
-            String jsonMessage = record.value();
-
-            // ObjectMapper를 사용하여 JSON 문자열을 Java 객체로 변환
-            TestDTO dto = objectMapper.readValue(jsonMessage, TestDTO.class);
-
-            log.info(dto.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @KafkaListener(topics = "sample-creator")
-    public void AssociationConsumer(ConsumerRecord<String, String> record) {
-        log.info("CONSUME TEST====================================================");
-        log.info("Consumed message: " + record);
-
-        try {
-            // 메시지 값을 JSON으로 받음
-            String jsonMessage = record.value();
-
-            // ObjectMapper를 사용하여 JSON 문자열을 Java 객체로 변환
-            AuthProducerDTO producerDTO = objectMapper.readValue(jsonMessage, AuthProducerDTO.class);
-            log.info(producerDTO.toString());
-
-            Long ano = Long.parseLong(producerDTO.getAuthCode().substring(6));
-            log.info(ano);
-            ApplierEntity applierEntity = applierEntityRepository.getApplier(ano).get();
-
-            if (applierEntity.getAuthCheck()){
-                String jsonString = objectMapper.writeValueAsString("checked");
-                associationProducer.sendReceiveAuthCheck(jsonString);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }//end try
-    }
+//    @KafkaListener(topics = "sample-creator")
+//    public void AssociationConsumer(ConsumerRecord<String, String> record) {
+//        log.info("CONSUME TEST====================================================");
+//        log.info("Consumed message: " + record);
+//
+//        try {
+//            // 메시지 값을 JSON으로 받음
+//            String jsonMessage = record.value();
+//
+//            // ObjectMapper를 사용하여 JSON 문자열을 Java 객체로 변환
+//            AuthProducerDTO producerDTO = objectMapper.readValue(jsonMessage, AuthProducerDTO.class);
+//            log.info(producerDTO.toString());
+//
+//            Long ano = Long.parseLong(producerDTO.getAuthCode().substring(6));
+//            log.info(ano);
+//            ApplierEntity applierEntity = applierEntityRepository.getApplier(ano).get();
+//
+//            if (applierEntity.getAuthCheck()){
+//                String jsonString = objectMapper.writeValueAsString("checked");
+//                associationProducer.sendReceiveAuthCheck(jsonString);
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }//end try
+//    }
 
     @KafkaListener(topics = "sample-association")
     public void ProducerConsumer(ConsumerRecord<String, String> record) {
@@ -76,12 +51,7 @@ public class KafkaConsumer {
         try {
             // 메시지 값을 JSON으로 받음
             String jsonMessage = record.value();
-
-            // ObjectMapper를 사용하여 JSON 문자열을 Java 객체로 변환
-//            AuthProducerDTO producerDTO = objectMapper.readValue(jsonMessage, AuthProducerDTO.class);
-
-            log.info("JSON = " + jsonMessage);
-
+            log.info(jsonMessage);
 
         } catch (Exception e) {
             e.printStackTrace();
